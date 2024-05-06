0f0101ebaf4ef93463056481821b627bd1dc96e0
Diego Dominguez <diegodgz.182@gmail.com>
2024-05-06 13:24:09 -0600

    Se añadió el método mostrarTabla

--- src/main/java/djdl/uv/Calculadora/CalculadoraApplication.java
+++ src/main/java/djdl/uv/Calculadora/CalculadoraApplication.java
@@ -1,13 +1,46 @@
 package djdl.uv.Calculadora;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class CalculadoraApplication {
+static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CalculadoraApplication.class, args);
-	}
+		int eleccion = mostrarMenu();
+        while(eleccion != 3){
+            switch (eleccion) {
+                case 1:
+                    mostrarTabla();
+                    break;
+                case 2:
+                    mostrarTablas();
+                    break;
+                case 3:
+                    break;
+            }
+        }
+    }
+    
+    public static int mostrarMenu(){
+
+        System.out.println("Bienvenido a las tablas de multiplicar");
+        System.out.println("1.- Mostrar una tabla");
+        System.out.println("2.- Mostrar tablas del 2 al 5");
+        System.out.println("3.- Salir");
+        int eleccion = scan.nextInt();
+        return eleccion;
+    }
+
+    public static void mostrarTabla(){
+        System.out.println("De qué número quieres la tabla?");
+        int eleccion = scan.nextInt();
+        for (int i = 0; i <= 10; i++){
+            System.out.println(eleccion + " * " + i + " = " + eleccion * i);
+        }
+    }
 
 }
